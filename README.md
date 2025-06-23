1c01ac2dd7c8d2f9902fbfff72c280b45628287e
betsekpoe <betsekpoe@st.knust.edu.gh>
2025-06-23 10:57:48 +0000

    Initial commit and add base class for File

---
+++ README.md
@@ -0,0 +1,1 @@
+# file_lengths_counter
---
+++ main.py
@@ -0,0 +1,15 @@
+class File:
+    def __init__(self, filename):
+        pass
+
+    def open_file(self):
+        pass
+
+    def word_count(self):
+        pass
+
+    def character_count(self):
+        pass
+
+    def line_count(self):
+        pass
