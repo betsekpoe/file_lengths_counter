a23e63693b98c7418f8825896205ea086f93a532
betsekpoe <betsekpoe@st.knust.edu.gh>
2025-06-23 13:13:49 +0000

    Implement file init & character count Added character count function for counting file both with and without whitespaces

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+from re import sub
+
 class File:
     def __init__(self, filename):
-        pass
+        self.file = open(filename, "r")
+        self.content = self.file.read()
 
-    def open_file(self):
+    def character_count(self, spaces = ""):
+        count = None
+        if spaces == "s".lower():
+            count =  len(self.content)
+        elif spaces == "":
+            count = len(sub(r"\s+", "", self.content))
+        return count
+
+    def line_count(self):
+        #line_count = 0
+        #for c in text:
+        #    if c == "\n":
+        #        line_count += 1
+        #line_count += 1"""
         pass
 
     def word_count(self):
         pass
 
-    def character_count(self):
+    def find_occurrences(self, word_to_find):
+        #print(f.count(word_to_find, 0))
         pass
 
-    def line_count(self):
-        pass
+file1 = File("dummy_text.txt")
+print(f"Character count (with spaces): {file1.character_count('s')}")
